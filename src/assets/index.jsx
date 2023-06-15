ee77ad980d5f0fc31e8b1a2baedc7f1c1dc33b2c
okaab29 <abubakar3@upi.edu>
2023-06-15 23:10:34 +0700

    (landingPage): update landing page

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,5 +1,17 @@
 import LogoLayout from "./img/layoutComponent/LogoLayout.svg";
-import BG_Login from "./img/AssetsLogin/BG_Login.png"
-import LogoHeader from "./img/landingPage/LogoHeader.svg"
-import AltaTech from "./img/landingPage/AltaTech.svg"
-export { LogoLayout, BG_Login, LogoHeader, AltaTech };
+import BG_Login from "./img/AssetsLogin/BG_Login.png";
+import LogoHeader from "./img/landingPage/LogoHeader.svg";
+import AltaTech from "./img/landingPage/AltaTech.svg";
+import Keyboard from "./img/landingPage/HeroSection/keyboard.svg";
+import Circle1 from "./img/landingPage/HeroSection/circle1.svg";
+import Circle2 from "./img/landingPage/HeroSection/circle2.svg";
+
+export {
+  LogoLayout,
+  BG_Login,
+  LogoHeader,
+  AltaTech,
+  Keyboard,
+  Circle1,
+  Circle2,
+};
